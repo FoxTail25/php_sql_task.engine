e95e6799ef85173a6d9083db37f4f89c46a3e67d
Fox Tail <foxtail25@gmail.com>
2025-07-03 18:03:26 +0300

    $_POST

--- public/view/pages/new_user.php
+++ public/view/pages/new_user.php
@@ -1,3 +1,9 @@
 <?php
+
+if(!empty($_POST['userName'])) {
+	var_dump($_POST);
+}
+
 $new_user = file_get_contents('view/baseview/new_user.html');
 return [ 'title' => 'new user', 'content' => $new_user];
+?>
