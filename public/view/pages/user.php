ec1882ad6c9c0008ab55090425f9dcedbb79ef08
Fox Tail <foxtail25@gmail.com>
2025-07-09 10:36:08 +0300

    Готово

--- public/view/pages/user.php
+++ public/view/pages/user.php
@@ -1,16 +1,16 @@
 <?php
-// $id = $_GET['id'];
-// $query = "SELECT * FROM users WHERE id = '$id'";
-// $userData = mysqli_fetch_assoc(mysqli_query($site_base_link, $query));
-// preg_match('#(\d*)-(\d*)-(\d*)#', $userData['birthdate'], $match);
-// $birthDate = $match[3].'-'.$match[2].'-'.$match[1];
-// $content = "
-// <div>
-// <p>Имя: $userData[name]</p>
-// <p>Фамилия: $userData[surname]</p>
-// <p>День Рождения: $birthDate</p>
-// </div>
-// ";
+$name = $params['slug'];
+$query = "SELECT * FROM users WHERE name = '$name'";
+$userData = mysqli_fetch_assoc(mysqli_query($site_base_link, $query));
+preg_match('#(\d*)-(\d*)-(\d*)#', $userData['birthdate'], $date);
+$birthDate = $date[3].'-'.$date[2].'-'.$date[1];
+$content = "
+<div>
+<p>Имя: $userData[name]</p>
+<p>Фамилия: $userData[surname]</p>
+<p>День Рождения: $birthDate</p>
+</div>
+";
 
-return ['title' => 'user_title', 'content' => 'user_content'];
+return ['title' => $userData['name'], 'content' => $content];
 ?>
